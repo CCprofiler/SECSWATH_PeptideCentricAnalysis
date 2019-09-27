ba0f6056f92359b61a8043bd2b3f65110e448277
Moritz Heusel <heusel@imsb.biol.imsb.ch>
2019-09-27 11:52:19 +0200

    Update Folder Setup & Cleanup, merged .osw analysis, with TRIC

--- SECSWATH_PeptideCentricAnalysis.sh
+++ SECSWATH_PeptideCentricAnalysis.sh
@@ -2,12 +2,13 @@
 
 ######################################################################
 # Docker-based OpenSwath+pyP+TRIC analysis of SEC-SWATH-MS data ######
-# Moritz Heusel 2019-08-19 ###########################################
+# Moritz Heusel 2019-09-06 ###########################################
 ######################################################################
 
 ######################################################################
 # STEP 1: Preparation: ###############################################
 ######################################################################
+
 # 00) Set up docker
 ### sudo snap install docker
 ### sudo usermod -a -G docker $USER
@@ -15,33 +16,37 @@
 docker run hello-world
 # get the containerized openswath tool(s)
 docker pull openswath/openswath:0.1.2
-## Spawn container with working directory mapped to /data/
-# remove containers that may exist in these names
-# watch out, modifications made inside these containers will be lost!
-docker stop openswath
-docker rm openswath
-# spawn containers on host machine
-docker run -u 0 -dit --name openswath -v $PWD/:/data openswath/openswath:0.1.2
-# say hello!
-docker exec openswath echo hi there, openswath container is happy and alive
-# Great, prepare input data:
-# 01) git clone and set up analysis framework:
-	git clone https://github.com/heuselm/SECSWATH_PeptideCentricAnalysis.git
-	cd SECSWATH_PeptideCentricAnalysis
-	mkdir data_dia
-	mkdir data_dia/unfractionated_secinput
+
+# 01) Now prepare data analysis folder(s) and input data:
+	# cd >myworkdir<; ca. 250 gb should be free
+git clone https://github.com/heuselm/SECSWATH_PeptideCentricAnalysis.git
+cd SECSWATH_PeptideCentricAnalysis
+
 # 02) Prepare Spectral/Peptide Query parameter Library:
 	# option 1: project-specific library:
 	# --> copy spectrast2tsv.tsv prepared as described in Schubert et al. 2015 to /data_library/
 	# option2: Use combined human library (Rosenberger 2014):
-	# --> donload link:
+	# --> download link:
 	# https://db.systemsbiology.net/sbeams/cgi/downloadFile.cgi?name=phl004_canonical_s64_osw.csv;format=tsv;tmp_file=8becf7ae782dd305c0eade59f282bcd1;raw_download=1
-	# move to /data_library/ and rename to spectrast2tsv.tsv
+	# Move to data_library and rename
+	# mv phl004_canonical_s64_osw.csv data_library/spectrast2tsv.tsv
 # 03) Prepare input data
+	mkdir data_dia
+	mkdir data/data_dia/unfractionated_secinput
 	# Convert .wiff to mzXML with peak picking / centroiding MS levels 1&2
 	# --> copy unfractionated sample SWATH64vw .mzXML to /data_dia/unfractionated_secinput/
 	# --> copy SEC fraction sample SWATH64vw .mzXMLs to /data_dia/
-######################################################################
+
+## Spawn OpenSwath container with working directory mapped to /data/
+
+# remove containers that may exist in these names
+# watch out, modifications made inside these containers will be lost!
+docker stop openswath
+docker rm openswath
+# spawn container
+docker run -u 0 -dit --name openswath -v $PWD/:/data openswath/openswath:0.1.2
+# say hello!
+docker exec openswath echo hi there, openswath container is happy and alive
 
 
 ######################################################################
@@ -53,17 +58,21 @@ docker exec openswath echo hi there, openswath container is happy and alive
 # docker exec works for OpenswathWorkflow but not PyProphet ##########
 ######################################################################
 docker attach openswath
+# press enter once to enter the bash commandline in the container
 
-# OpenSwathWorkflow
+# Set up result folders
 mkdir /data/results
 mkdir /data/results/openswath
 mkdir /data/results/openswath/unfractionated_secinput
 
 # Convert líbrary to .pqp
-TargetedFileConverter -in /data/data_library/spectrast2tsv.tsv -out /data/data_library/spectrast2tsv.pqp
+TargetedFileConverter -in /data/data_library/spectrast2tsv.tsv \
+-out /data/data_library/spectrast2tsv.pqp
 # Generate decoys
-OpenSwathDecoyGenerator -in /data/data_library/spectrast2tsv.pqp -out /data/data_library/spectrast2tsv_td.pqp
+OpenSwathDecoyGenerator -in /data/data_library/spectrast2tsv.pqp \
+-out /data/data_library/spectrast2tsv_td.pqp
 
+## OpenSwathWorkflow
 # Run OpenSwath on unfractionated sample(s)
 for file in /data/data_dia/unfractionated_secinput/*ML; do \
 bname=$(echo ${file##*/} | cut -f 1 -d '.'); \
@@ -78,7 +87,7 @@ OpenSwathWorkflow \
 -rt_extraction_window 600 \
 -mz_extraction_window 30 \
 -ppm \
--threads 12 \
+-threads 6 \
 -use_ms1_traces \
 -Scoring:Scores:use_ms1_mi \
 -Scoring:Scores:use_mi_score ; done
@@ -97,41 +106,29 @@ OpenSwathWorkflow \
 -rt_extraction_window 600 \
 -mz_extraction_window 30 \
 -ppm \
--threads 12 \
+-threads 6 \
 -use_ms1_traces \
 -Scoring:Scores:use_ms1_mi \
 -Scoring:Scores:use_mi_score ; done
+
 
 ######################################################################
 # STEP3: PYPROPHET: ##################################################
 # Score and filter OpenSwath results #################################
 ######################################################################
 # Note: This is the "new" pipeline training a global model ###########
-# that is then applied to each individual run (stabilized srocing) ###
-# Error models are learned and applied on ############################
-# precursor, peptide and protein level ###############################
+# that is then applied to each individual run (stabilized scoring) ###
 ######################################################################
-# create result folders
+# create pyprophet result folders
 mkdir /data/results/pyprophet
 mkdir /data/results/pyprophet/unfractionated_secinput
 
-# Train Model: Subsample over all runs and create jumbo model
-################################################
-# pyprophet merge --out=/data/results/pyprophet/unfractionated_secinput/subsampled.osw \
-# --subsample_ratio=0.5 /data/results/openswath/*.osw
-# pyprophet score --threads 4 --in=/data/results/pyprophet/unfractionated_secinput/subsampled.osw \
-# --level=ms1ms2
-# # for inspection by humans:
-# pyprophet export --in=/data/results/pyprophet/unfractionated_secinput/subsampled.osw \
-# --out=/data/results/pyprophet/unfractionated_secinput/subsampled.tsv --format=legacy_merged \
-# --no-ipf
-
-# Train Model: pyProphet analysis of unfractionated sample 
+# Train Global Model: pyProphet analysis of unfractionated sample 
 #####################################################################
 pyprophet score --threads 6 --in=/data/results/openswath/unfractionated_secinput/unfractionated_secinput.osw \
 --out=/data/results/pyprophet/unfractionated_secinput/model.osw --level=ms1ms2
 
-# Apply: Score all runs using the unfractionated model
+# Apply global model to score peak groups in all runs evenly
 #####################################################################
 # Merge
 pyprophet merge --out=/data/results/pyprophet/allruns.osw \
@@ -143,39 +140,17 @@ pyprophet score --threads 6 --in=/data/results/pyprophet/allruns.osw \
 
 # Export results before pyProphet Qvalue/FDR estimation
 pyprophet export --in=/data/results/pyprophet/allruns.osw \
---out=/data/results/pyprophet/allruns_preQvalEstn.tsv --format=legacy_merged \
---no-ipf
-
-# FDR estimation:
-#####################################################################
-# Create statistical models on peptide and protein level and over
-# different contexts (Within each run or over the full dataset)
-#####################################################################
-# Peptide level
-pyprophet \
-peptide --in=/data/results/pyprophet/allruns.osw --context=run-specific \
-peptide --in=/data/results/pyprophet/allruns.osw --context=experiment-wide \
-peptide --in=/data/results/pyprophet/allruns.osw --context=global \
-# Protein level
-pyprophet \
-protein --in=/data/results/pyprophet/allruns.osw --context=run-specific \
-protein --in=/data/results/pyprophet/allruns.osw --context=experiment-wide \
-protein --in=/data/results/pyprophet/allruns.osw --context=global \
-
-# Export results with experiment-wide and global Qvalue estimation:
-pyprophet export --in=/data/results/pyprophet/allruns.osw \
---out=/data/results/pyprophet/allruns_pepQvals_protQvals.tsv --format=legacy_merged \
+--out=/data/results/pyprophet/allruns.tsv --format=legacy_merged \
 --no-ipf
 
 pyprophet export --in=/data/results/pyprophet/allruns.osw \
---out=/data/results/pyprophet/allruns_pepQvals_protQvals_matrix.tsv --format=matrix \
+--out=/data/results/pyprophet/allruns_matrix.tsv --format=matrix \
 --no-ipf
 
 # export scoring pdf reports
 pyprophet export \
 --in=/data/results/pyprophet/allruns.osw \
 --format=score_plots
-
 
 
 ######################################################################
@@ -185,10 +160,11 @@ pyprophet export \
 ######################################################################
 # Roest et al. https://www.nature.com/articles/nmeth.3954 ############
 ######################################################################
+# create TRIC result folder
 mkdir /data/results/TRIC
 
 feature_alignment.py \
---in /data/results/pyprophet/allruns_pepQvals_protQvals.tsv \
+--in /data/results/pyprophet/allruns.tsv \
 --out /data/results/TRIC/feature_alignment.tsv \
 --out_matrix /data/results/TRIC/feature_alignment_matrix.tsv \
 --method LocalMST \
@@ -196,10 +172,14 @@ feature_alignment.py \
 --max_rt_diff 60 \
 --mst:useRTCorrection True \
 --mst:Stdev_multiplier 3.0 \
---target_fdr 0.05 \
---fdr_cutoff 0.01
+--target_fdr 0.05
 
+# exit the docker container
 exit
+
+# clean up
+docker stop openswath
+docker rm openswath
 
 # Congrats, you ran the OpenSwathPipeline!
 # Check output in results/ folder
